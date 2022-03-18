e3f90afed451f3f91fd0eeaca203cd6a148c53b0
moni-tu <turani.monica@gmail.com>
2022-03-18 11:46:06 +0100

    add test "render list of events"

--- src/__tests__/App.test.js
+++ src/__tests__/App.test.js
@@ -3,5 +3,9 @@ import { shallow } from 'enzyme';
 import App from '../App';
 
 describe('<App /> component', () => {
+    test('render list of events', () => {
+        const AppWrapper = shallow(<App />);
+        expect(AppWrapper.find(EventList)).toHaveLength(1);
+      });
 
 });
