df6bc2e3533df8649d7a302cdfc2e29273df1071
moni-tu <turani.monica@gmail.com>
2022-03-18 11:45:49 +0100

    fix: add class instead of function

--- src/App.js
+++ src/App.js
@@ -1,12 +1,11 @@
 import React, { Component } from 'react';
 import './App.css';
 
-function App extends Component() {
+class App extends Component {
   render() {
     return (
       <div className="App">
-        
-        
+
       </div>
     );
   }
