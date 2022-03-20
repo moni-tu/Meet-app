07cb1a91dbe2306366f4a0f1eac2643284cae62e
moni-tu <turani.monica@gmail.com>
2022-03-20 23:00:13 +0100

    add test "selecting a suggestion should change query state"

--- src/__tests__/CitySearch.test.js
+++ src/__tests__/CitySearch.test.js
@@ -53,6 +53,14 @@ describe('<CitySearch /> component', () => {
           return location.toUpperCase().indexOf(query.toUpperCase()) > -1;
         });
         expect(CitySearchWrapper.state("suggestions")).toEqual(filteredLocations);
-      });
+    });
+
+    test("selecting a suggestion should change query state", () => {
+        CitySearchWrapper.setState({
+          query: 'Berlin'  });
+        const suggestions = CitySearchWrapper.state('suggestions');
+        CitySearchWrapper.find('.suggestions li').at(0).simulate('click');
+        expect(CitySearchWrapper.state("query")).toBe(suggestions[0]);
+    });
     
 })
