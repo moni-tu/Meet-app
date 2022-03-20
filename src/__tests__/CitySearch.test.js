4d9cc600e3b01a9a5116f88daf72f3d00f3d8250
moni-tu <turani.monica@gmail.com>
2022-03-20 21:47:00 +0100

    add test: renders text input correctly

--- src/__tests__/CitySearch.test.js
+++ src/__tests__/CitySearch.test.js
@@ -3,12 +3,20 @@ import { shallow } from 'enzyme';
 import CitySearch from '../CitySearch';
 
 describe('<CitySearch /> component', () => {
-  test('render text input', () => {
+    test('render text input', () => {
     const CitySearchWrapper = shallow(<CitySearch />);
     expect(CitySearchWrapper.find('.city')).toHaveLength(1);
-  });
-  test ('render a list of suggestions', () => {
+    });
+
+    test ('render a list of suggestions', () => {
     const CitySearchWrapper = shallow(<CitySearch />);
     expect(CitySearchWrapper.find('.suggestions')).toHaveLength(1);
     });
+
+    test('renders text input correctly', () => {
+        const CitySearchWrapper = shallow(<CitySearch />);
+        const query = CitySearchWrapper.state('query');
+        expect(CitySearchWrapper.find('.city').prop('value')).toBe(query);
+    });
+
 });
