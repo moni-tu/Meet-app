61ee1d2b72729c4bf3e64c480999b1ad636b76f9
moni-tu <turani.monica@gmail.com>
2022-03-21 10:54:33 +0100

    fix: add curly brackets to mockData and fix typo in 'length"

--- src/__tests__/EventList.test.js
+++ src/__tests__/EventList.test.js
@@ -2,11 +2,11 @@ import React from 'react';
 import { shallow } from 'enzyme';
 import EventList from '../EventList';
 import Event from "../Event"
-import mockData from "../mock-data"
+import { mockData } from "../mock-data"
 
 describe('<EventList /> component', () => {
   test('render correct number of events', () => {
-    const EventListWrapper = shallow(<EventList events={{mockData}} />);
-    expect(EventListWrapper.find(Event)).toHaveLength(mockData.lenght);
+    const EventListWrapper = shallow(<EventList events={mockData} />);
+    expect(EventListWrapper.find(Event)).toHaveLength(mockData.length);
   });
 });
