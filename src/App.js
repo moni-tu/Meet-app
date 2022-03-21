7771f72a7dbac4f57fbc1c1a3b9d03378c895a5a
moni-tu <turani.monica@gmail.com>
2022-03-21 14:56:19 +0100

    create events state and pass it as a prop
    Create events state in the App component
    Pass events state to the EventList component as a prop of events

--- src/App.js
+++ src/App.js
@@ -5,12 +5,17 @@ import CitySearch from './CitySearch';
 import NumberOfEvents from './NumberOfEvents';
 
 class App extends Component {
+
+  state = {
+    events: []
+  }
+
   render() {
     return (
       <div className="App">
         <CitySearch/>
         <EventList />
-        <NumberOfEvents />
+        <NumberOfEvents events={this.state.events}/>
 
       </div>
     );
