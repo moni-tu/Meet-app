b13db46c5d29f95f1acdb8c120e8c8d16f2638cc
moni-tu <turani.monica@gmail.com>
2022-03-21 10:51:42 +0100

    feat: add state, handleclick, event summary, start time, timeyone , location and onClick function

--- src/Event.js
+++ src/Event.js
@@ -1,9 +1,32 @@
-import react, { Component } from "react";
+import React, { Component } from "react";
 
 class Event extends Component {
+
+    state = {
+        collapsed: true
+    }
+    handleClick = () => {
+        this.setState({
+            collapsed: !this.state.collapsed
+        });
+    };
+
     render() {
+
+        const { event } = this.props;
+        const { collapsed } = this.state;
+
         return 
-        <div></div>
+        <div className="event">
+            <h2 className="summary">{event.summary}</h2>
+            <p className="start-date">
+            {event.start.dateTime} ({event.start.timeZone})
+            </p>
+            <p className="location">
+                {event.location}
+            </p>
+            <button className={`${collapsed ? "show" : "hide"}-details`} onClick={this.handleClick}>{collapsed ? "Show Details" : "Hide Details"}</button>
+        </div>
     }
 }
 
