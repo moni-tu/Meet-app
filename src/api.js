91461b36d8beb547e5ccff20dce9d31e9dd6ab32
moni-tu <turani.monica@gmail.com>
2022-03-22 21:05:45 +0100

    fix: move getAccessToken UP on line 18

--- src/api.js
+++ src/api.js
@@ -15,6 +15,27 @@ import NProgress from 'nprogress';
     return locations;
   };
 
+  export const getAccessToken = async () => {
+    const accessToken = localStorage.getItem('access_token');
+    const tokenCheck = accessToken && (await checkToken(accessToken));
+
+    if (!accessToken || tokenCheck.error) {
+      await localStorage.removeItem("access_token");
+      const searchParams = new URLSearchParams(window.location.search);
+      const code = await searchParams.get("code");
+      if (!code) {
+        const results = await axios.get(
+          "https://sdji75aqzf.execute-api.eu-central-1.amazonaws.com/dev/api/get-auth-url"
+        );
+        const { authUrl } = results.data;
+        return (window.location.href = authUrl);
+      }
+      return code && getToken(code);
+    }
+    return accessToken;
+  }
+
+
   const checkToken = async (accessToken) => {
     const result = await fetch(
       `https://www.googleapis.com/oauth2/v1/tokeninfo?access_token=${accessToken}`
@@ -27,10 +48,6 @@ import NProgress from 'nprogress';
   };
 
   export const getEvents = async () => {
-
-    if (window.location.href.startsWith('http://localhost')) {
-      return mockData;
-    }
 
     NProgress.start();
     if (window.location.href.startsWith("http://localhost")) {
@@ -66,26 +83,6 @@ import NProgress from 'nprogress';
       window.history.pushState("", "", newurl);
     }
   };
-
-  export const getAccessToken = async () => {
-    const accessToken = localStorage.getItem('access_token');
-    const tokenCheck = accessToken && (await checkToken(accessToken));
-
-    if (!accessToken || tokenCheck.error) {
-      await localStorage.removeItem("access_token");
-      const searchParams = new URLSearchParams(window.location.search);
-      const code = await searchParams.get("code");
-      if (!code) {
-        const results = await axios.get(
-          "https://sdji75aqzf.execute-api.eu-central-1.amazonaws.com/dev/api/get-auth-url"
-        );
-        const { authUrl } = results.data;
-        return (window.location.href = authUrl);
-      }
-      return code && getToken(code);
-    }
-    return accessToken;
-  }
 
   const getToken = async (code) => {
     const encodeCode = encodeURIComponent(code);
