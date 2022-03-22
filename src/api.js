df854c9517da86537a0d12ee54a734d54ceb02ab
moni-tu <turani.monica@gmail.com>
2022-03-22 12:40:12 +0100

    add function getAccessToken with get auth url from Lambda

--- src/api.js
+++ src/api.js
@@ -19,5 +19,22 @@ import axios from 'axios';
   };
 
   export const getAccessToken = async () => {
+    const accessToken = localStorage.getItem('access_token');
+    const tokenCheck = accessToken && (await checkToken(accessToken));
+
+    if (!accessToken || tokenCheck.error) {
+      await localStorage.removeItem("access_token");
+      const searchParams = new URLSearchParams(window.location.search);
+      const code = await searchParams.get("code");
+      if (!code) {
+        const results = await axios.get(
+          "https://sdji75aqzf.execute-api.eu-central-1.amazonaws.com/dev/api/get-auth-url"
+        );
+        const { authUrl } = results.data;
+        return (window.location.href = authUrl);
+      }
+      return code && getToken(code);
+    }
+    return accessToken;
 
   }
