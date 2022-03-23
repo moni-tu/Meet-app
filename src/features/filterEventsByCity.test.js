2cb0121090446b3027d6b3e3b6fc962290077d3a
moni-tu <turani.monica@gmail.com>
2022-03-23 21:19:51 +0100

    style: fix spacing

--- src/features/filterEventsByCity.test.js
+++ src/features/filterEventsByCity.test.js
@@ -12,6 +12,7 @@ defineFeature(feature, test => {
         given('user hasn’t searched for any city', () => {
 
         });
+
         let AppWrapper;
         when('the user opens the app', () => {
             
@@ -22,43 +23,45 @@ defineFeature(feature, test => {
         });
 
         then('the user should see the list of upcoming events.', () => {
+            AppWrapper.update();
+            expect(AppWrapper.find('.event')).toHaveLength(mockData.length);
+        });
+    });    
+
+    test('User should see a list of suggestions when they search for a city', ({ given, when, then }) => {
+        given('the main page is open', () => {
 
         });
 
-        test('User should see a list of suggestions when they search for a city', ({ given, when, then }) => {
-            given('the main page is open', () => {
-    
-            });
-    
-            when('the user starts typing in the city textbox', () => {
-    
-            });
-    
-            then('the user should receive a list of cities (suggestions) that match what they’ve typed', () => {
-    
-            });
+        when('the user starts typing in the city textbox', () => {
+
         });
 
-        test('User can select a city from the suggested list', ({ given, and, when, then }) => {
-            given('user was typing “Berlin” in the city textbox', () => {
-    
-            });
-    
-            and('the list of suggested cities is showing', () => {
-    
-            });
-    
-            when('the user selects a city (e.g., “Berlin, Germany”) from the list', () => {
-    
-            });
-    
-            then('their city should be changed to that city (i.e., “Berlin, Germany”)', () => {
-    
-            });
-    
-            and('the user should receive a list of upcoming events in that city', () => {
-    
-            });
+        then('the user should receive a list of cities (suggestions) that match what they’ve typed', () => {
+
         });
     });
+
+    test('User can select a city from the suggested list', ({ given, and, when, then }) => {
+        given('user was typing “Berlin” in the city textbox', () => {
+
+        });
+
+        and('the list of suggested cities is showing', () => {
+
+        });
+
+        when('the user selects a city (e.g., “Berlin, Germany”) from the list', () => {
+
+        });
+
+        then('their city should be changed to that city (i.e., “Berlin, Germany”)', () => {
+
+        });
+
+        and('the user should receive a list of upcoming events in that city', () => {
+
+        });
+    });
+
 });
