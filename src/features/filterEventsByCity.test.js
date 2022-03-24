7b83425c8ba90a79778d5f95d9cf263b7bfb6f9d
moni-tu <turani.monica@gmail.com>
2022-03-24 08:39:35 +0100

    fix: delete .js from "api" in imported data
    Also add line 10 const locations = extractLocations(mockData); otherwise locations was not defined.
    Add locations in variables line 30.
    Style: reduce spacing line 19-23.

--- src/features/filterEventsByCity.test.js
+++ src/features/filterEventsByCity.test.js
@@ -4,23 +4,20 @@ import { mount, shallow } from 'enzyme';
 import App from '../App';
 import { mockData } from '../mock-data';
 import CitySearch from '../CitySearch';
-import { extractLocations } from '../api.js';
+import { extractLocations } from "../api";
 
 const feature = loadFeature('./src/features/filterEventsByCity.feature');
+const locations = extractLocations(mockData);
 
 defineFeature(feature, test => {
 
     test('When user hasn’t searched for a city, show upcoming events from all cities.', ({ given, when, then }) => {
         given('user hasn’t searched for any city', () => {
-
         });
 
         let AppWrapper;
-        when('the user opens the app', () => {
-                    
-            AppWrapper = mount(<App />);
-            
-
+        when('the user opens the app', () => {      
+            AppWrapper = mount(<App />);        
         });
 
         then('the user should see the list of upcoming events.', () => {
@@ -30,17 +27,18 @@ defineFeature(feature, test => {
     });    
 
     test('User should see a list of suggestions when they search for a city', ({ given, when, then }) => {
-        let CitySearchWrapper;
+        let CitySearchWrapper, locations;
         given('the main page is open', () => {
             CitySearchWrapper = shallow(<CitySearch updateEvents={() => {}} locations={locations} />);
         });
 
         when('the user starts typing in the city textbox', () => {
+            /* CitySearchWrapper.find('.city').simulate('change', { target: { value: 'Berlin' } }); */
 
         });
 
         then('the user should receive a list of cities (suggestions) that match what they’ve typed', () => {
-
+            /* expect(CitySearchWrapper.find('.suggestions li')).toHaveLength(2); */
         });
     });
 
