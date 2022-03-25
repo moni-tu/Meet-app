675953ad54550150195926f129a4f1beaed85887
moni-tu <turani.monica@gmail.com>
2022-03-25 17:30:37 +0100

    comment out "filter events by city" test
    It is part of an extra exercise you don`t have to do

--- src/__tests__/EndToEnd.test.js
+++ src/__tests__/EndToEnd.test.js
@@ -1,6 +1,6 @@
 import puppeteer from "puppeteer";  
 
-describe('show/hide an event details', () => {
+ describe('show/hide an event details', () => {
     let browser;
     let page;
     jest.setTimeout(30000);
@@ -37,4 +37,30 @@ describe('show/hide an event details', () => {
         const eventDetails = await page.$('.event .event__Details');
         expect(eventDetails).toBeNull();
       });
-});
+}); 
+
+/* describe('filter events by city', () => {
+    let browser;
+    let page;
+    jest.setTimeout(30000);
+    beforeAll(async () => {
+      
+       browser = await puppeteer.launch({
+        headless: false,
+        slowMo: 250, // slow down by 250ms
+       ignoreDefaultArgs: ['--disable-extensions'] // ignores default setting that causes timeout errors
+      });
+      page = await browser.newPage();
+      await page.goto('http://localhost:3000/');
+      await page.waitForSelector('.event');
+    });
+
+    afterAll(() => {
+        browser.close();
+    });
+
+    test("When user hasn’t searched for a city, show upcoming events from all cities", async () => {
+        const eventList = await page.$('.EventList');
+        expect(eventList).toBe(2);
+    });
+});*/
