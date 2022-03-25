f4dc10830183981f3df3219142b52354e9dfd8e3
moni-tu <turani.monica@gmail.com>
2022-03-25 12:42:03 +0100

    fix: add test User can expand an event to see its details
    Add correct class name show-details instead of details-button

--- src/__tests__/EndToEnd.test.js
+++ src/__tests__/EndToEnd.test.js
@@ -17,5 +17,18 @@ describe('show/hide an event details', () => {
         const eventDetails = await page.$('.event .event__Details');
         expect(eventDetails).toBeNull();
         browser.close();
-      });
+    });
+
+    test('User can expand an event to see its details', async () => {
+        const browser = await puppeteer.launch();
+        const page = await browser.newPage();
+        await page.goto('http://localhost:3000/');
+    
+        await page.waitForSelector('.event');
+        await page.click('.event .show-details');
+    
+        const eventDetails = await page.$('.event .event__Details');
+        expect(eventDetails).toBeDefined();
+        browser.close();
+    });
 });
