bd741d404ddaff8214f0701fe6d3d56ad4d4dca4
moni-tu <turani.monica@gmail.com>
2022-03-25 12:46:09 +0100

    add third scenario with correct btn class name: hide-details

--- src/__tests__/EndToEnd.test.js
+++ src/__tests__/EndToEnd.test.js
@@ -27,4 +27,10 @@ describe('show/hide an event details', () => {
         const eventDetails = await page.$('.event .event__Details');
         expect(eventDetails).toBeDefined();
     });
+
+    test('User can collapse an event to hide its details', async () => {
+        await page.click('.event .hide-details');
+        const eventDetails = await page.$('.event .event__Details');
+        expect(eventDetails).toBeNull();
+      });
 });
