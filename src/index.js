35d20257d9fac3fb9c0875118f6f588e3ea44739
moni-tu <turani.monica@gmail.com>
2022-03-27 18:23:12 +0200

    install. import and call atatus
    Use unique config code given by atatus in config.install

--- src/index.js
+++ src/index.js
@@ -4,6 +4,8 @@ import './index.css';
 import App from './App';
 import * as serviceWorkerRegistration from './serviceWorkerRegistration';
 import reportWebVitals from './reportWebVitals';
+import * as atatus from 'atatus-spa';
+atatus.config('a83b6729c3ac417e9c63971813e9cc5e').install();
 
 ReactDOM.render(
   <React.StrictMode>
@@ -21,3 +23,5 @@ serviceWorkerRegistration.unregister();
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
+
+atatus.notify(new Error('Test Atatus Setup'));
