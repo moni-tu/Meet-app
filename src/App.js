fc4441743d9d4d15fb05404f9112e68edbb37203
moni-tu <turani.monica@gmail.com>
2022-04-06 00:11:17 +0200

    feat: add welcome screen if statement

--- src/App.js
+++ src/App.js
@@ -52,15 +52,22 @@ class App extends Component {
 
   async componentDidMount() {
     this.mounted = true;
-    getEvents().then((events) => {
-      if (this.mounted) {
-        this.setState({ 
+    const accessToken = localStorage.getItem('access_token');
+    const isTokenValid = (await checkToken(accessToken)).error ? false : true;
+    const searchParams = new URLSearchParams(window.location.search);
+    const code = searchParams.get("code");
+    this.setState({ showWelcomeScreen: !(code || isTokenValid) });
+    if ((code || isTokenValid) && this.mounted) {
+      getEvents().then((events) => {
+        if (this.mounted) {
+          this.setState({ 
           events: events.slice(0, this.state.numberOfEvents),
           locations: extractLocations(events),
-        });
-      }
-    });
-  }
+          });
+        }
+      });
+    }
+  };
   componentWillUnmount(){
     this.mounted = false;
   }
@@ -74,6 +81,7 @@ class App extends Component {
         <EventList 
         events={this.state.events} 
         />
+        <WelcomeScreen showWelcomeScreen={this.state.showWelcomeScreen}getAccessToken={() => { getAccessToken() }} />
         
       </div>
     );
