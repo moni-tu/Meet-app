d7320d457793d422bd3cee0bd0ac0ecbb5090c17
moni-tu <turani.monica@gmail.com>
2022-04-10 15:50:54 +0200

    fix: fix welcome screen button

--- src/WelcomeScreen.jsx
+++ src/WelcomeScreen.jsx
@@ -16,8 +16,7 @@ import './WelcomeScreen.css';
                             <div className="google-icon-wrapper">
                             <img
                             className="google-icon"
-                            src="https://upload.wikimedia.org/wikipedia/commons/5/53/Google_%22G%22_Log
-                            o.svg"
+                            src="https://upload.wikimedia.org/wikipedia/commons/5/53/Google_%22G%22_Logo.svg"
                             alt="Google sign-in"
                             />
                             </div>
